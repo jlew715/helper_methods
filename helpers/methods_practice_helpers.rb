06a6e7e2914e93e4135da056317ace11d59e152c
Jerold Lewis <jeroldlewis@Jerolds-MacBook-Air.local>
2015-03-14 02:42:52 -0400

    done with helper methods

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -1,22 +1,18 @@
 module MethodsPracticeHelpers
 
   def image_tag(source, alternate_text)
-    # Generate an image tag with the given source and the classes
-    # "img-responsive" and "img-thumbnail". The image should also have an alt
-    # attribute with the contents of alternate_text.
+    return "<img src=\""+@cat_image.url+"\" class=\"img-responsive img-thumbnail\" alt=\""+alternate_text+"\">"
+    # My 20-week old kitten really enjoyed watching me test this one
   end
 
   def bootstrap_alert(message, type='info')
-    # Generates the HTML for a Bootstrap alert (useful since it can be diffcult
-    # to remember all HTML needed to do it right). It should show the message in
-    # the "message" variable and should have a class appropriate for the "type"
-    # that is passed in, defaulting to 'info' if no type is passed in.
+    return "<div class=\"alert alert-"+type+"\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times;</a>"+message+"</div>"
   end
 
   def current_date_and_time
-    # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
-    # Time.now.strftime is a great method chain you can use for this purpose.
-    # See how to use it at: http://apidock.com/ruby/Time/strftime
+    curTime = Time.new
+    return curTime.strftime("%B").to_s+" "+curTime.day.to_s+", "+curTime.year.to_s+" at "+curTime.strftime("%I:%M%p").to_s
+
   end
 
   # ------------------------------------------
